c6b5012f95b05bd89c24f5be50a0805132f24d34
sneha10yadav <sneha10yadav@gmail.com>
2021-05-09 19:11:23 +0530

    created structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
   return (
     <div>
+      <div className="header">
+        Phone Directory
+      </div>
+      <div className="content">
+        <button>Add</button><br></br>
+        <label htmlFor="name" >Name:</label>
+        <input type="text" defaultValue="Sneha Yadav"></input>
+      </div>
     </div>
   );
 }
